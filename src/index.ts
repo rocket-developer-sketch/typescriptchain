f38f8db019f877d102edf0ee99476266f603dc03
nyk <drivingdeveloper@gmail.com>
2021-04-19 20:09:52 +0900

    #make blockchain. index is one because it is not adding a block to a blockchain

--- src/index.ts
+++ src/index.ts
@@ -39,16 +39,41 @@ const genesisBlock:Block= new Block(0, "20202020202", "", "Hello", 123456);
 // an array of Block
 let blockchain: Block[] = [genesisBlock];
 
+const getBlockchain = (): Block[] => blockchain;
 
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+
+const createNewBlock = (data:string) : Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(
+        newIndex, 
+        previousBlock.hash, 
+        newTimestamp, 
+        data
+    );
+
+    const newBlock: Block = new Block(
+        newIndex, 
+        newHash, 
+        previousBlock.hash, 
+        data, 
+        newTimestamp
+    );
+
+    return newBlock;
+}
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 // block 은 hash를 가짐
 // hash : 모든 속성을 엄청 길고 수학적으로 이상한 하나의 문자열로 결합한 것
 
 // crpyto-js 설치
 // npm install crypto-js
-
-console.log(blockchain);
 
     export{};
     
