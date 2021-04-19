a08a9f245d64aac84b92e658e02a0602a8f691e2
nyk <drivingdeveloper@gmail.com>
2021-04-19 17:31:53 +0900

    #how to use Typescript. remove one of parameters from function sayHi. Then Typescript tell compling error

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+const name = "Nicolas",
+    age = 24,
+    gender = "male";
+
+
+const sayHi = (name, age, gneder) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age, gender);
+
+    // Cannot reduclare block-scoped variable 'name' 해결법으로 export{} 입력(버그 중 하나라고 예상)
+    export{};
