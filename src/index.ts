63df4d9fb2f5c92a9abdf1c3a52367cfc8456a48
nyk <drivingdeveloper@gmail.com>
2021-04-19 19:57:56 +0900

    #modified the way to make an array with an object. line 40

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,10 @@ class Block {
 const genesisBlock:Block= new Block(0, "20202020202", "", "Hello", 123456);
 
 // an array of Block
-let blockchain:[Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+
+
 
 // block 은 hash를 가짐
 // hash : 모든 속성을 엄청 길고 수학적으로 이상한 하나의 문자열로 결합한 것
