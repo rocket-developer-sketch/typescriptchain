f34acb5e164d1461fd3ef3ae8a3322d63d5c32d8
nyk <drivingdeveloper@gmail.com>
2021-04-19 19:45:22 +0900

    #when type is an array of a class

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
-// 인터페이스는 javascript로 컴파일 되지 않음.
-// 그렇기 때문에 class 사용
-// 속성 선언, 속성들이 가진 권한 정해줘야 함.
-// Java의 접근제어자 같이 사용
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-
-    constructor(name:string, age:number, gender:string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index:number,
+        hash: string, 
+        previousHash: string,
+        data: string,
+        timestamp: number
+        )
+        {
+            this.index = index;
+            this.hash = hash;
+            this.previousHash = previousHash;
+            this.data = data;
+            this.timestamp = timestamp;
     }
 }
 
-const lynn = new Human("Lynn", 18, "female");
+const genesisBlock:Block= new Block(0, "20202020202", "", "Hello", 123456);
 
-// typescript가 person 이 Human 인터페이스와 같은 형태인지 체크하기 때문에
-// object로 argument 전달 가능
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+// an array of Block
+let blockchain:[Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
+console.log(blockchain);
 
-    // Cannot reduclare block-scoped variable 'name' 해결법으로 export{} 입력(버그 중 하나라고 예상)
     export{};
     
