e2f3606f24d00a8b56a5b83134af99b75ef61a0f
nyk <drivingdeveloper@gmail.com>
2021-04-19 19:31:40 +0900

    #optional parameter

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ class Human {
     public age: number;
     public gender: string;
 
-    constructor(name:string, age:number, gender:string){
+    constructor(name:string, age:number, gender?:string){
         this.name = name;
         this.age = age;
         this.gender = gender;
