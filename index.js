bfbed20170b1a23f420dd38c23ee988469250445
nyk <drivingdeveloper@gmail.com>
2021-04-19 18:06:28 +0900

    #add return type on function

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Nicolas", age = 24, gender = "male";
 const sayHi = (name, age, gneder) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    return `Hello ${name}, you are ${age}, you are a ${gneder}`;
 };
-sayHi(name, age, gender);
+console.log(sayHi("Nicolas", 44, "male"));
 //# sourceMappingURL=index.js.map
