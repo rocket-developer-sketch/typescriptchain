bf12546048f87da8c56ab8dbff08f874d7fdfbca
nyk <drivingdeveloper@gmail.com>
2021-04-19 19:23:38 +0900

    fix typo. person.gneder -> person.gender

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const person = {
 // typescript가 person 이 Human 인터페이스와 같은 형태인지 체크하기 때문에
 // object로 argument 전달 가능
 const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gneder}`;
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
 console.log(sayHi( person));
