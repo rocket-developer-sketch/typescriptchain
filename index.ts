57c8839bf5fc4d22daf73a1fed424e12ff16c803
nyk <drivingdeveloper@gmail.com>
2021-04-19 17:32:49 +0900

    #parameter + ? = selective parameter

--- index.ts
+++ index.ts
@@ -3,11 +3,11 @@ const name = "Nicolas",
     gender = "male";
 
 
-const sayHi = (name, age, gneder) => {
+const sayHi = (name, age, gneder?) => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 }
 
-sayHi(name, age, gender);
+sayHi(name, age);
 
     // Cannot reduclare block-scoped variable 'name' 해결법으로 export{} 입력(버그 중 하나라고 예상)
     export{};
