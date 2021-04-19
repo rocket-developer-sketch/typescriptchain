875c7b693f3b22b49057777370493ebb04ee4e51
nyk <drivingdeveloper@gmail.com>
2021-04-19 19:31:05 +0900

    #use classes on Typescript when it is needed replacement of interface for Javascript for a reason

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+// 인터페이스는 javascript로 컴파일 되지 않음.
+// 그렇기 때문에 class 사용
+// 속성 선언, 속성들이 가진 권한 정해줘야 함.
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
 
+    constructor(name:string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
-const person = {
-    name : "Nicolas",
-    age : 22,
-    gender : "male"
-    
-};
+
+const lynn = new Human("Lynn", 18, "female");
 
 // typescript가 person 이 Human 인터페이스와 같은 형태인지 체크하기 때문에
 // object로 argument 전달 가능
@@ -17,7 +21,7 @@ const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi( person));
+console.log(sayHi(lynn));
 
     // Cannot reduclare block-scoped variable 'name' 해결법으로 export{} 입력(버그 중 하나라고 예상)
     export{};
