3942e624ea7341ba368ae5abb4587b8e20cfc7d8
nyk <drivingdeveloper@gmail.com>
2021-04-19 19:19:12 +0900

    #interface on Typescript. pass an object to function as an argument

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-const sayHi = (name:string, age:number, gneder:string): string => {
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+
+}
+const person = {
+    name : "Nicolas",
+    age : 22,
+    gender : "male"
+    
+};
+
+// typescript가 person 이 Human 인터페이스와 같은 형태인지 체크하기 때문에
+// object로 argument 전달 가능
+const sayHi = (person: Human): string => {
     return `Hello ${name}, you are ${age}, you are a ${gneder}`;
 };
 
-console.log(sayHi( "Nicolas", 44, "male"));
+console.log(sayHi( person));
 
     // Cannot reduclare block-scoped variable 'name' 해결법으로 export{} 입력(버그 중 하나라고 예상)
     export{};
