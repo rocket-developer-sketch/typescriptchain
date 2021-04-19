cf71982c444b35e6dea3d47fa510ce9ae8db110d
nyk <drivingdeveloper@gmail.com>
2021-04-19 19:21:04 +0900

    #use attributes from an object argument thtat type is interface

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const person = {
 // typescript가 person 이 Human 인터페이스와 같은 형태인지 체크하기 때문에
 // object로 argument 전달 가능
 const sayHi = (person: Human): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gneder}`;
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gneder}`;
 };
 
 console.log(sayHi( person));
